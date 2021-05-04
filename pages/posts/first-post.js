c4ccec76585c4d75b0c4f897f4c9b6784599185a
edsoncarlosdvp <edsoncarlos@ynail.com>
2021-05-03 23:38:44 -0300

    Finalizado

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
+import Layout from '../../components/Layout'
+
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
+                <link rel="icon" href="/favicon.ico" />
             </Head>
             <h1>First Post</h1>
             <h2>
@@ -13,6 +16,6 @@ export default function FirstPost() {
                     <a>Back to home</a>
                 </Link>
             </h2>
-        </>
+        </Layout>
     )
 }
