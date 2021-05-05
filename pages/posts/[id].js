c1c23d6faa63598b7e0b4b7d1413d73089951dbd
edsoncarlosdvp <edsoncarlos@ynail.com>
2021-05-05 17:26:32 -0300

    Rotas Dinâmicas => Implementar getStaticPaths

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,6 +4,31 @@ export default function Post() {
     return <Layout>...</Layout>
 }
 
+export function getAllPostIds() {
+    const fileNames = fs.readdirSync(postsDirectory)
+
+    // Returns an array that looks like this:
+    // [
+    //   {
+    //     params: {
+    //       id: 'ssg-ssr'
+    //     }
+    //   },
+    //   {
+    //     params: {
+    //       id: 'pre-rendering'
+    //     }
+    //   }
+    // ]
+    return fileNames.map(fileName => {
+        return {
+            params: {
+                id: fileName.replace(/\.md$/, '')
+            }
+        }
+    })
+}
+
 export async function getStaticPaths() {
     // Return a list of possible value for id
 }
